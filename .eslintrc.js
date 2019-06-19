56385b20ce538e0fc6b1b1b51f351d8c7e269f6a
Craig <craigdanj@gmail.com>
2019-06-19 21:00:25 +0530

    reformatted prettier rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true
-  },
-  'extends': [
-    'plugin:prettier/recommended',
-    'plugin:react/recommended'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+    'env': {
+        'browser': true,
+        'es6': true
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
-  },
-  'plugins': [
-    'react',
-    'prettier'
-  ],
-  'rules': {
-    'prettier/prettier': 'error'
-  }
+    'extends': [
+        'plugin:prettier/recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'prettier'
+    ],
+    'rules': {
+        'prettier/prettier': 'error'
+    }
 }
