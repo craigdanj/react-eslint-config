4baca4ea331d7588a0c51ba80a761074e2a42d02
Craig <craigdanj@gmail.com>
2019-06-22 15:20:37 +0530

    react rules added

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,35 @@ module.exports = {
     ],
     'rules': {
         'prettier/prettier': 2,
+
+        //React Rules
+        'react/button-has-type': 2,
+        'react/destructuring-assignment': 2,
+        'react/forbid-component-props': 2,
+        'react/no-access-state-in-setstate': 2,
+        'react/no-did-mount-set-state': 2,
+        'react/no-did-update-set-state': 2,
+        'react/no-direct-mutation-state': 2,
+        'react/no-typos': 2,
+        'react/no-this-in-sfc': 2,
+        'react/no-unescaped-entities': 2,
+        'react/no-unused-state': 2,
+        'react/no-will-update-set-state': 2,
+        'react/prefer-stateless-function': 2,
+        'react/require-render-return': 2,
+        'react/self-closing-comp': 2,
+        'react/sort-comp': [2, {
+            order: [
+                'constructor',
+                'everything-else',
+                'lifecycle',
+                'render'
+            ]
+        }],
+        'react/state-in-constructor': [2, 'never'],
+        'react/void-dom-elements-no-children': 2,
+
+        //JSX Rules
         'react/jsx-boolean-value': 2,
         'react/jsx-closing-bracket-location': 2,
         'react/jsx-closing-tag-location': 2,
@@ -43,7 +72,7 @@ module.exports = {
         'react/jsx-curly-brace-presence': 2,
         'react/jsx-pascal-case': 2,
         'react/jsx-props-no-multi-spaces': 2,
-        'jsx-tag-spacing': {
+        'react/jsx-tag-spacing': {
             'closingSlash': 'never',
             'beforeSelfClosing': 'always',
             'afterOpening': 'never',
@@ -51,7 +80,10 @@ module.exports = {
         },
         'react/jsx-uses-react': 2,
         'react/jsx-uses-vars': 2,
-        'jsx-wrap-multilines': 2
+        'react/jsx-wrap-multilines': 2,
 
+
+        // Optional: Uncomment only if you think you need them
+        // 'react/require-optimization': 2,
     }
 }
